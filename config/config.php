f88cdd33aee43947201f71d27a72102ac679efd7
Pawel Malicki <Pawel.Malicki@nibe.se>
2019-04-03 19:58:40 +0200

    add latest files

--- config/config.php
+++ config/config.php
@@ -5,3 +5,4 @@
   define('DEFAULT_CONTROLLER', 'Home'); // Default controller if there is not one defined in the url
   define('DEFAULT_LAYOUT', 'default'); // If no layout is set the controller use this layout
   define('SITE_TITLE', 'MVC Framework'); // this will be used if no site is set
+  
