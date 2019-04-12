b23ff88ef0a7a742adbba1b0044a16e48655267f
Pawel Malicki <Pawel.Malicki@nibe.se>
2019-04-13 00:18:03 +0200

    Refactor Session 1 done

--- config/config.php
+++ config/config.php
@@ -5,4 +5,3 @@
   define('DEFAULT_CONTROLLER', 'Home'); // Default controller if there is not one defined in the url
   define('DEFAULT_LAYOUT', 'default'); // If no layout is set the controller use this layout
   define('SITE_TITLE', 'MVC Framework'); // this will be used if no site is set
-  
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php
 
-  session_start();
   define('DS', DIRECTORY_SEPARATOR);
   define('ROOT', dirname(__FILE__));
 
-  $url = [];
-  if(isset($_SERVER['PATH_INFO'])) {
-    $url = explode('/', ltrim($_SERVER['PATH_INFO'], '/'));
+  // Load configuration and helper functions
+  require_once(ROOT . DS . 'config' . DS . 'config.php');
+  require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+  // Autoload classes
+  function autoload($className) {
+    if(file_exists(ROOT . DS . 'core' . DS . $className . '.php')) {
+      require_once(ROOT . DS . 'core' . DS . $className . '.php');
+    } elseif (file_exists(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php')) {
+      require_once(ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php');
+    } elseif (file_exists(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php')) {
+      require_once(ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php');
+    }
   }
 
-  require_once(ROOT. DS . 'core' . DS . 'bootstrap.php');
+  spl_autoload_register('autoload');
+
+  session_start();
+
+  // Route the request
+  $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+  Router::route($url);
