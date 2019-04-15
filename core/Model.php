f5043dc25372c3d3c4160fb721553a6c9447a6b2
Pawel Malicki <Pawel.Malicki@nibe.se>
2019-04-15 14:51:16 +0200

    Model Class 2 done

--- core/Model.php
+++ core/Model.php
@@ -26,4 +26,32 @@
     public function getColumns() {
       return $this->_db->getColumns($this->_table);
     }
+
+    public function find($params = []) {
+      $results = [];
+      $resultsQuery = $this->_db->find($this->_table, $params);
+      foreach($resultsQuery as $result) {
+        $obj = new $this->_modelName($this->_table);
+        $obj->populateObjData($result);
+        $results[] = $obj;
+      }
+      return $results;
+    }
+
+    public function findFirst($params = []) {
+      $resultQuery = $this->_db->findFirst($this->_table, $params);
+      $result = new $this->_modelName($this->_table);
+      $result->populateObjData($resultQuery);
+      return $result;
+    }
+
+    public function findById($id) {
+      return $this->findFirst(['conditions' => 'id = ?', 'bind' => [$id]]);
+    }
+
+    protected function populateObjData($result) {
+      foreach($result as $key => $val) {
+        $this->$key = $val;
+      }
+    }
   }
