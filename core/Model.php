f87b38496554b5abb1e100e005a02c39c7938a66
Pawel Malicki <Pawel.Malicki@nibe.se>
2019-04-25 11:06:50 +0200

    Model Class 3

--- core/Model.php
+++ core/Model.php
@@ -7,6 +7,7 @@
     protected $_modelName;
     protected $_softDelete = false;
     protected $_columnNames = [];
+    public $id;
 
     public function __construct($table) {
       $this->_db = DB::getinstance();
@@ -47,6 +48,61 @@
 
     public function findById($id) {
       return $this->findFirst(['conditions' => 'id = ?', 'bind' => [$id]]);
+    }
+
+    public function save() {
+      $fields = [];
+      foreach($this->_columnNames as $column) {
+        $fields[$column] = $this->$column;
+      }
+      if(property_exists($this, 'id') && $this->id != '') {
+        return $this->update($this->id, $fields);
+      } else {
+        return $this->insert($fields);
+      }
+    }
+
+    public function insert($fields) {
+      if(empty($fields)) return false;
+      return $this->_db->insert($this->_table, $fields);
+    }
+
+    public function update($id, $fields) {
+      if(empty($fields) || $id == '') return false;
+      return $this->_db->update($this->_table, $id, $fields);
+    }
+
+    public function delete($id) {
+      if($id == '' && $this->id == '') return false;
+      $id = ($id == '') ? $this->id : $id;
+      if($this->_softDelete) {
+        return $this->update($id, ['deleted' => 1]);
+      }
+      return $this->_db->delete($this->_table, $id);
+    }
+
+    public function query($sql, $bind = []) {
+      return $this->_db->query($sql, $bind);
+    }
+
+    public function data() {
+      $data = new stdClass();
+      foreach($this->_columnNames as $column) {
+        $data->column = $this->column;
+      }
+      return $data;
+    }
+
+    public function assign($params) {
+      if(!empty($params)) {
+        foreach($params as $key => $val) {
+          if(in_array($key, $this->_columnNames)) {
+            $this->$key = sanitize($val);
+          }
+        }
+        return true;
+      }
+      return false;
     }
 
     protected function populateObjData($result) {
