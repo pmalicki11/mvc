d50628d69d211b4fd8d2ff6a1ba4efce0696c4e5
Pawel Malicki <Pawel.Malicki@nibe.se>
2019-05-15 18:16:20 +0200

    User Sessions Model Refactor done

--- app/models/UserSessions.php
+++ app/models/UserSessions.php
@@ -6,4 +6,18 @@
       $table = 'user_sessions';
       parent::__construct($table);
     }
+
+    public static function getFromCookie() {
+      $userSession = new self();
+      if(Cookie::exists(REMEMBER_ME_COOKIE_NAME)) {
+        $userSession = $userSession->findFirst([
+          'conditions' => 'user_agent = ? AND session = ?',
+          'bind' => [Session::uagent_no_version(), Cookie::get(REMEMBER_ME_COOKIE_NAME)]
+        ]);
+      }
+      if(!$userSession) {
+        return false;
+      }
+      return $userSession;
+    }
   }
